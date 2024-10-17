509b98e07b3c1e116687bbced3d0cf31f19df28d
xyxyxyxxxy <162995032+xyxyxyxxxy@users.noreply.github.com>
2024-10-17 17:18:27 +0800

    Update single_metrics.sh

--- single_metrics.sh
+++ single_metrics.sh
@@ -1,3 +1,1 @@
-python metrics.py --model_paths /data/code0516/Scaffold-GS-hybrid-DC-barf/outputs/tandt/train/baseline/2024-08-15_09:08:22-opti-pose
-
-#python metrics.py --model_paths /data/code0516/Scaffold-GS-hybrid-DC-barf/outputs/tandt/train/baseline/2024-08-19_08:18:19-opti-pose-false
+python metrics.py --model_paths /data/tant/train
